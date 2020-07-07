b6d5361b956dfbf2769e3e295a501aaabda50157
Your Namekinderzyc <75829215@qq.com>
2020-07-07 13:25:29 +0800

    修改nav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <router-view />
+    <hr />
+    <div>
+      <router-link to="/money">记账</router-link>
+      |
+      <router-link to="/lables">标签</router-link>
+      |
+      <router-link to="/statistics">统计</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
 <style lang="scss">
+@import "./assets/style/test.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/Lables.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>   
+        Lables.vue
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        name: 'Lables'
+    })
+</script>
+
+<style scoped>
+
+</style>

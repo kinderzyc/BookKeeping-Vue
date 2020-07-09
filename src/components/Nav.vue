185a2fb381790c350d31665e1ea478286dd64189
Your Namekinderzyc <75829215@qq.com>
2020-07-09 15:26:15 +0800

    ICON

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="nav">
-    <router-link to="/lables">标签</router-link>|
-    <router-link to="/money">记账</router-link>|
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/lables">
+      <Icon name="lables" />标签
+    </router-link>
+    <router-link to="/money">
+      <Icon name="wallet" />记账
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="charts" />统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+// let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+//   try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+
+// let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+
 export default {
   name: "Nav"
 };

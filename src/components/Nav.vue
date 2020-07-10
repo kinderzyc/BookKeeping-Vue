d024ea0a49496417642a0b4d4b6654720b65c48c
Your Namekinderzyc <75829215@qq.com>
2020-07-10 11:46:35 +0800

    Nav样式添加

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/lables">
+  <nav>
+    <router-link to="/lables" class="item">
       <Icon name="lables" />标签
     </router-link>
-    <router-link to="/money">
+    <router-link to="/money" class="item">
       <Icon name="wallet" />记账
     </router-link>
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="charts" />统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
-// let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-//   try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
-
-// let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 
 export default {
   name: "Nav"
@@ -24,7 +20,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
-}
+  nav{
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    font-size: 16px;
+    flex-direction: row;
+    
+        > .item {
+            padding: 6px 0;
+            width: 33.3333%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            .icon{
+                width: 32px;
+                height: 32px;
+            }
+        }
+  }
 </style>

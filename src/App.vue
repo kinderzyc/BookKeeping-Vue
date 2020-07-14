79322a3c5b3027f81da31fc16bbf7c45f5867776
Your Namekinderzyc <75829215@qq.com>
2020-07-15 05:07:45 +0800

    html和css编写

--- src/App.vue
+++ src/App.vue
@@ -10,37 +10,14 @@ export default Vue.extend({});
 </script>
 
 <style lang="scss">
-@import "./assets/style/test.scss";
- * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  body{
-    line-height: 1.5;
-  }
-  a{
-    text-decoration: none;
-    color: inherit;
-  }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import "./assets/style/reset.scss";
+@import "./assets/style/helper.scss";
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  line-height: 1.5;
+  font-family: $font-black;
+  font-size: 20px;
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,12 @@
 <template>
   <nav>
-    <router-link to="/lables" class="item">
+    <router-link to="/lables" class="item" active-class="selected">
       <Icon name="lables" />标签
     </router-link>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="wallet" />记账
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected"  >
       <Icon name="charts" />统计
     </router-link>
   </nav>
@@ -37,6 +37,9 @@ export default {
                 width: 32px;
                 height: 32px;
             }
+        }
+        .item.selected{
+          color: #003399;
         }
   }
 </style>

959a396ca47ea30d2090e974af76ff9d9bab0d4c
Your Namekinderzyc <75829215@qq.com>
2020-07-15 15:06:45 +0800

    模块化：把money分为块组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="请在这里输入备注" />
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Notes"
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  background: #f5f5f5;
+  font-size: 20px;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+    font-weight: bolder;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tags">
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Tags"
+};
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  font-size: 20px;
+  padding: 16px;
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      background: #d9d9d9;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 26px;
+      margin-right: 12px;
+      margin-top: 4px;
+      font-weight: bold;
+    }
+  }
+  > .new {
+    padding-top: 18px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+</style>

e9f85ab73a27a5a153e4f5b69340ce2403d6c75d
Your Namekinderzyc <75829215@qq.com>
2020-07-18 17:20:18 +0800

    修改

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="tags">
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li
+        v-for="tag in dataSource"
+        :key="tag"
+        :class="{selected: selectedTags.indexOf(tag)>=0}"
+        @click="toggle(tag)"
+      >{{tag}}</li>
     </ul>
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: "Tags"
-};
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Tags extends Vue {
+  @Prop() dataSource: string[] | undefined;
+  selectedTags: string[] = [];
+
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+  }
+  create() {
+    const name = window.prompt("请输入标签名");
+    if (name === "") {
+      window.alert("标签名不能为空！");
+    } else if (this.dataSource) {
+      this.$emit("update:dataSource", [...this.dataSource, name]);
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -29,15 +53,20 @@ export default {
     display: flex;
     flex-wrap: wrap;
     > li {
-      background: #d9d9d9;
+      $bg: #d9d9d9;
+      background: $bg;
       $h: 28px;
       height: $h;
       line-height: $h;
       border-radius: $h/2;
       padding: 0 26px;
       margin-right: 12px;
-      margin-top: 4px;
+      margin-top: 12px;
       font-weight: bold;
+      &.selected {
+        background: darken($bg, 40%);
+        color: white;
+      }
     }
   }
   > .new {

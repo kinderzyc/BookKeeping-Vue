c6cc23aedf1542f879d366900d92a7cb28e0f036
Your Namekinderzyc <75829215@qq.com>
2020-08-12 15:15:30 +0800

    Tags.vue使用vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -21,15 +21,17 @@ import { Component, Prop } from "vue-property-decorator";
 
 @Component({
   computed: {
-    tagList(){
-      //todo
-    //  return this.$store.fetchTags();
-    return []
+    tagList() {
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit("fetchTags");
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -45,8 +47,7 @@ export default class Tags extends Vue {
     if (!name) {
       return window.alert("标签名不能为空！");
     }
-    //todo
-    // store.createTag(name);
+     this.$store.commit("createTag", name);
   }
 }
 </script>

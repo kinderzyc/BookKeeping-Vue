e7dd13448869e0227a63dab8cadfeb1587fe09ff
Your Namekinderzyc <75829215@qq.com>
2020-08-14 13:16:28 +0800

    EditLabel.vue 使用vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -24,11 +24,13 @@ import Button from "../components/Button.vue";
   components: { Notes, Button }
 })
 export default class EditLabel extends Vue {
-  tag?: Tag = undefined;
+  get tag() {
+    return this.$store.state.currentTag;
+  }
 
   created() {
-    //todo
-    // this.tag = store.findTag(this.$route.params.id);
+    const id = this.$route.params.id;
+    this.$store.commit("setCurrentTag", id);
     if (!this.tag) {
       this.$router.replace("/404");
     }
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -16,21 +16,19 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Button from "../components/Button.vue";
-// import store from "../store/index2";
+import { mixins } from "vue-class-component";
+import TagHelper from "../mixins/TagHelper";
 
 @Component({
-  components: {Button}
+  components: { Button }
 })
-export default class Lables extends Vue {
-  // tags = store.tagList;
-  tags = [] //todo
-
-  createTag() {
-    const name = window.prompt("请输入标签名");
-    if (name) {
-      //todo
-      // store.createTag(name);
-    }
+export default class Labels extends mixins(TagHelper) {
+  $store;
+  get tags() {
+    return this.$store.state.tagList;
+  }
+  beforeCreate() {
+    this.$store.commit("fetchTags");
   }
 }
 </script>

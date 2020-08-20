9f029141c4201c06bd66525b354272ec82315c66
Your Namekinderzyc <75829215@qq.com>
2020-08-20 17:19:49 +0800

    在Money.vue使用Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
-    <Types :value.sync="record.type" />
+    <Tabs :data-source="recordTypeList" :value.sync="record.type" />
     <Notes field-name="备注" placeholder="请输入备注名" @update:value="onUpdateNotes" />
     <Tags />
   </Layout>
@@ -10,21 +10,22 @@
 <script lang="ts">
 import Vue from "vue";
 import NumberPad from "../components/Money/NumberPad.vue";
-import Types from "../components/Money/Types.vue";
 import Notes from "../components/Money/Notes.vue";
 import Tags from "../components/Money/Tags.vue";
+import Tabs from "../components/Tabs.vue";
 import { Component } from "vue-property-decorator";
 import store from "../store/index";
+import recordTypeList from "../constants/recordTypeList";
 
 @Component({
-  components: { NumberPad, Types, Notes, Tags },
-  computed: {
-    recordList() {
-      return this.$store.state.recordList;
-    }
-  }
+  components: { NumberPad, Tabs, Notes, Tags }
 })
 export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+  recordTypeList = recordTypeList;
+
   record: RecordItem = {
     tags: [],
     notes: "",
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,31 +1,25 @@
 <template>
   <Layout>
-    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type" />
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="hhh" />
+    <Tabs class-prefix="type" :data-source="recordTypeLis" :value.sync="type" />
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" />
   </Layout>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import Types from "../components/Money/Types.vue";
 import Tabs from "../components/Tabs.vue";
+import intervalList from "../constants/intervalList";
+import recordTypeLis from "../constants/recordTypeList";
 
 @Component({
-  components: { Tabs, Types }
+  components: { Tabs }
 })
 export default class Statistics extends Vue {
   type = "-";
-  hhh = "day";
-  intervalList = [
-    { text: "按天", value: "day" },
-    { text: "按周", value: "week" },
-    { text: "按月", value: "month" }
-  ];
-  typeList = [
-    {text: "支出", value: "-"},
-    {text: "收入", value: "+"}
-  ];
+  interval = "day";
+  intervalList = intervalList;
+  recordTypeLis = recordTypeLis;
 }
 </script>
 
